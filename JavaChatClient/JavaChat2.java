5d41fc62c39e3216d1a4100aa9055d989a4a5b68
Andre Durant <56659807+adurant101@users.noreply.github.com>
2021-11-25 19:41:14 -0800

    Add files via upload

---
+++ JavaChatClient/JavaChat2.java
@@ -0,0 +1,12 @@
+package javachat2;
+
+public class JavaChat2 {
+
+    public static void main(String[] args) {
+        //run client gui
+        ChatFrame chat = new ChatFrame();
+        chat.setVisible(true);
+    }
+
+
+}
---
+++ JavaChatClient/intMessage.java
@@ -0,0 +1,6 @@
+package javachat2;
+
+//abstract class used to display messages to client gui
+public interface intMessage {
+    public void setServerMessage(String m);
+}
